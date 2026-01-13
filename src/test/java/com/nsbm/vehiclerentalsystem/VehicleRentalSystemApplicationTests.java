8047af13e7a6a4115e6425b82ac59157b108c229
chathushkaimasara <chathushkapromax@gmail.com>
2026-01-13 21:10:18 +0530

    Initial commit

---
+++ src/test/java/com/nsbm/vehiclerentalsystem/VehicleRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.vehiclerentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
